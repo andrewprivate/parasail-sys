18d20e44b891b6914357e294add72cecb6c1c26f
Andrew S <andrews3596@gmail.com>
2021-06-03 16:19:17 -0400

    Cleanup

--- build.rs
+++ build.rs
@@ -3,10 +3,6 @@
 // This software may be modified and distributed under the terms of the MIT license.  See the
 // LICENSE file for details.
 
-use std::env;
-use std::fs;
-use std::path::Path;
-use std::process::Command;
 extern crate cmake;
 
 fn main() {
@@ -14,7 +10,7 @@ fn main() {
     let dst = cmake::Config::new("parasail_c").define("BUILD_SHARED_LIBS","OFF").profile("Release").build();
     let path = dst.join("build");
     let outpath;
-    if (path.join("Release").exists()) {
+    if path.join("Release").exists() {
         outpath = path.join("Release");
     } else {
         outpath = path;
